2b29765fbaa268b9181997d37f0ea918d12ab59b
Dima Tytenko <amgpetronass@gmail.com>
2022-07-04 21:44:56 +0300

    add

--- sendgrid/helpers/sendEmail.js
+++ sendgrid/helpers/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (email, token) => {
   const mail = {
     to: email,
     subject: "Service Contacts",
-    html: `<a target="_blank" href="http://localhost:${process.env.PORT}/api/users/verify/${token}">Verification your email</a>`,
-    from: "amgpetronass@gmail.com",
+    html: `<a target="_blank" href="${process.env.SENDGRID_HOST}${process.env.PORT}/api/users/verify/${token}">Verification your email</a>`,
+    from: process.env.SENDGRID_FROM,
   };
 
   try {
